b5cd0ae1dd157d0c0c783efdf032b465761e4c71
Matheus Victor <hffmatheus@gmail.com>
2016-07-03 11:09:41 -0300

    ajustes

--- Views/minhainterface.class.php
+++ Views/minhainterface.class.php
@@ -94,7 +94,8 @@ Abstract Class MinhaInterface
             <html>
             <head>
             <title>{$this->titulo}</title>
-            <meta charset='utf-8'>
+            <meta charset=\"utf-8\">
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
                  <link rel='stylesheet' type='text/css' href='../CSS/EvCSS.css'
 
             </head>
